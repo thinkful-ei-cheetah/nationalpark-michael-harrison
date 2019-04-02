db38f29159619ea71dcc8e7c88c34bb30c57add6
Michael Verdi <michael.v.verdi@gmail.com>
2019-04-02 17:43:59 -0400

    happy path of the NPS search results is working

--- index.js
+++ index.js
@@ -1,34 +1,66 @@
-'use strict'
-const store=[];
+'use strict';
 
+let store = [];
 
-//'api_key= 'hDamnHcByq8RvYZzTPXAbI157CfyqWHEigdtEsDg'
+class Park {
+  constructor(data) {
+    this.fullName = data.fullName;
+    this.url = data.url;
+    this.description = data.description;
+  }
+}
+
 function watchForm(){
-    $('form#js-form').on('submit', (event)=>{
-        event.preventDefault();
-        const state=$('#js-search-term').val();
-        const maxNumber=$('#js-max-results').val();
-
-    })
+  $('form#js-form').on('submit', (event)=>{
+    event.preventDefault();
+    const stateCodes=$('#js-search-term').val();
+    const maxNumber=$('#js-max-results').val();
+    getParks(stateCodes, maxNumber);
+  });
 }
 function formatQueryParams(params){
-    const queryItems= Object.keys(params).map(key=>`${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
-    return queryItems.join('&');
+  const queryItems= Object.keys(params).map(key=>`${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+  return queryItems.join('&');
 }
+
+function buildApiUrl(query, limit) {
+  const baseUrl = 'https://developer.nps.gov/api/v1/parks';
+  const apiCode = 'hDamnHcByq8RvYZzTPXAbI157CfyqWHEigdtEsDg';
+  let queryParam = {
+    limit: limit,
+    stateCode: query,
+    api_key: apiCode   
+  };
+  return baseUrl + '?' + formatQueryParams(queryParam);
+}
+
+function buildParkHtml(park) {
+  return `
+    <li>
+      <a href='${park.url}' target='_blank'>${park.fullName}</a>
+      <p>${park.description}</p>
+    </li>
+  `;
+}
+
+function render() {
+  const parkTemplate = store.map(buildParkHtml);
+  $('#results-list').html(parkTemplate);
+  $('#results').removeClass('hidden');
+}
+
 function getParks(query,limit){
-    const baseUrl='https://developer.nps.gov/api/v1/parks';
-    const apiCode ='hDamnHcByq8RvYZzTPXAbI157CfyqWHEigdtEsDg';
-    let queryParam={
-        limit:limit,
-        stateCode:query,
-        api_key:apiCode   
-    };
-    let finalurl = baseurl+formatQueryParams(queryParam);
-    
+  const apiUrl = buildApiUrl(query, limit);
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(jsonData => {
+      store = jsonData.data.map(data => new Park(data));
+      render();
+    });
 }
 
 function main(){
-    watchForm();
-    getParks()
+  watchForm();
 }
-$(main());
+
+$(main);
